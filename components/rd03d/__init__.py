51aab4ff3aa5eb5afcdd7e86ebbf922568dc2926
kw112021246 <161500171+kw112021246@users.noreply.github.com>
2025-04-23 11:43:09 -0600

    Update __init__.py

--- components/rd03d/__init__.py
+++ components/rd03d/__init__.py
@@ -1,22 +1,25 @@
-from esphome.components import sensor, uart
-import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_UART_ID, CONF_NAME
-from esphome.core import coroutine_with_priority
 import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor, uart
+from esphome.const import CONF_ID, UNIT_METERS, ICON_RULER, DEVICE_CLASS_DISTANCE, STATE_CLASS_MEASUREMENT
 
-DEPENDENCIES = ["uart"]
+rd03d_ns = cg.esphome_ns.namespace('rd03d')
+RD03DSensor = rd03d_ns.class_('RD03DSensor', cg.PollingComponent, uart.UARTDevice)
 
-rd03d_ns = cg.esphome_ns.namespace("rd03d")
-RD03DSensor = rd03d_ns.class_("RD03DSensor", sensor.Sensor, cg.PollingComponent, uart.UARTDevice)
-
-CONFIG_SCHEMA = sensor.sensor_schema().extend({
+CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(RD03DSensor),
-    cv.GenerateID(CONF_UART_ID): cv.use_id(uart.UARTComponent),
-}).extend(cv.polling_component_schema("1s"))
+    cv.Required("uart_id"): cv.use_id(uart.UARTComponent),
+    cv.Required("name"): cv.string,
+}).extend(sensor.sensor_schema(
+    unit_of_measurement=UNIT_METERS,
+    icon=ICON_RULER,
+    accuracy_decimals=2,
+    device_class=DEVICE_CLASS_DISTANCE,
+    state_class=STATE_CLASS_MEASUREMENT,
+))
 
-@coroutine_with_priority(40.0)
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID], config[CONF_UART_ID])
+    var = cg.new_Pvariable(config[CONF_ID], config["uart_id"])
     await cg.register_component(var, config)
-    await sensor.register_sensor(var, config)
-    await uart.register_uart_device(var, config[CONF_UART_ID])
+    sens = await sensor.new_sensor(config)
+    cg.add(var.set_sensor(sens))
